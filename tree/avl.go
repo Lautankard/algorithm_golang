4fb30441f78092550e1b96eca0a29d08abde46be
liujingbo <jingbo_liu@qingsongchou.com>
2020-06-30 22:45:10 +0800

    comment

--- tree/avl.go
+++ tree/avl.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type AvlNode struct {
 	Data  int
-	Hight int
+	Hight int //标记节点的高度
 	Left  *AvlNode
 	Right *AvlNode
 }
@@ -24,6 +24,7 @@ func Max(a, b int) int {
 	return b
 }
 
+//节点的平衡因子
 func TreeBalanceFactor(root *AvlNode) int {
 	if root == nil {
 		return 0
@@ -81,6 +82,7 @@ func TreeBalanceFactor(root *AvlNode) int {
 // }
 // }
 
+//左旋
 func TreeRotateLeft(rootPtr **AvlNode) {
 	root := *rootPtr
 	if root == nil || root.Right == nil {
@@ -90,10 +92,12 @@ func TreeRotateLeft(rootPtr **AvlNode) {
 	root.Right = right.Left
 	right.Left = root
 	*rootPtr = right
+	//旋转后需要重新计算被旋转节点的hight值
 	root.Hight = TreeHight(root)
 	right.Hight = TreeHight(right)
 }
 
+//右旋
 func TreeRotateRight(rootPtr **AvlNode) {
 	root := *rootPtr
 	if root == nil || root.Left == nil {
@@ -110,14 +114,14 @@ func TreeRotateRight(rootPtr **AvlNode) {
 func TreeRebalance(rootPtr **AvlNode) {
 	root := *rootPtr
 	factor := TreeBalanceFactor(root)
-	if factor > 1 && TreeBalanceFactor(root.Left) > 0 {
+	if factor > 1 && TreeBalanceFactor(root.Left) > 0 { //LL 左左：左高右低，数据插入到了左子树的左子树，只需要右旋
 		TreeRotateRight(rootPtr)
-	} else if factor > 1 && TreeBalanceFactor(root.Left) <= 0 {
+	} else if factor > 1 && TreeBalanceFactor(root.Left) <= 0 { //LR, 左高右低，数据插在了左子树的右子树上，需要先左转再右转
 		TreeRotateLeft(&root.Left)
 		TreeRotateRight(rootPtr)
-	} else if factor < -1 && TreeBalanceFactor(root.Right) <= 0 {
+	} else if factor < -1 && TreeBalanceFactor(root.Right) <= 0 { //RR, 左低右高，数据插在了右子树的右子树，只需要左旋
 		TreeRotateLeft(rootPtr)
-	} else if factor < -1 && TreeBalanceFactor(root.Right) > 0 {
+	} else if factor < -1 && TreeBalanceFactor(root.Right) > 0 { //RL,左低右高，数据插在了右子树的左子树，需要先右旋再左旋
 		TreeRotateRight(&root.Right)
 		TreeRotateLeft(rootPtr)
 	}
@@ -138,6 +142,7 @@ func TreeInsert(rootPtr **AvlNode, value int, b bool) {
 	} else {
 		TreeInsert(&root.Right, value, b)
 	}
+	//插入节点重新计算所影响节点的height
 	root.Hight = TreeHight(root)
 	if b {
 		TreeRebalance(rootPtr)
